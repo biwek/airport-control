51febbcddf63fdc4302f71ea3461f801831318ef
biwek <biweks@gmail.com>
2014-01-24 13:35:17 +0000

    added test and methods for planes being landed, taken off and if airport is full

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,7 +1,22 @@
 class Airport 
 
+	DEFAULT_CAPACITY = 10
+
 	def initialize(plane = [])
 		@planes = plane
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
+	def planes
+		@planes
+	end
+
+	def capacity
+		@capacity
+	end
+
+	def capacity=(value)
+		@capacity = value
 	end
 
 	def has_planes?
@@ -9,11 +24,16 @@ class Airport
 	end
 
 	def plane_landed(plane)
-		@planes = plane.land
+		raise "Sorry, Airport is FULL!!" if full?
+		@planes << plane
 	end
 
 	def plane_taken_off(plane)
-		@planes = plane.take_off
+		@planes.delete(plane)
+	end
+
+	def full?
+		@planes.count == @capacity
 	end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,39 +3,33 @@ require_relative '../lib/airport'
 describe Airport do  
 
 	let(:airport) {Airport.new}
+
+	def fill_airport(planes)
+		airport.capacity.times { airport.plane_landed(planes)}
+	end
 	
 	it "should not contain planes" do  
 		expect(airport).not_to have_planes
 	end
 
-	it "should allow planes to land" do 
+	it "should contain a plane after it has landed" do 
 		planes = double :planes
-		expect(planes).to receive(:land)
 		airport.plane_landed(planes)
-	end
-
-	it "should contain a plane after it has landed" do  
-		airport1 = Airport.new(:plane)
-		planes = double :planes
-		expect(planes).to receive(:land)
-		airport.plane_landed(planes)
-		expect(airport1).to have_planes
-	end
-
-	it "should allow planes to take off" do 
-		airport1 = Airport.new(:plane)
-		planes = double :planes
-		expect(airport1).to have_planes
-		expect(planes).to receive(:take_off)
-		airport1.plane_taken_off(planes)
+		expect(airport).to have_planes
+		expect(airport.planes.count).to eq(1)
 	end
 
 	it "should not contain a plane after it has taken off" do 
 		planes = double :planes
-		airport1 = Airport.new(:plane)
-		expect(planes).to receive(:take_off)
-		airport1.plane_taken_off(planes)
-		expect(airport1).not_to have_planes
+		airport.plane_taken_off(planes)
+		expect(airport).not_to have_planes
+		expect(airport.planes.count).to eq(0)
+	end
+
+	it "should not allow planes to land if full" do 
+		planes = double :planes
+		fill_airport(planes)
+		expect { airport.plane_landed(planes) }.to raise_error("Sorry, Airport is FULL!!")
 	end
 
 end
